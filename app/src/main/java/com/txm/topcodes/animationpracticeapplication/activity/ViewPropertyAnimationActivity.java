76f995be0a1c116a9751c11c2bb0f2ba6b83fc36
kimball <tangxianming1994@gmail.com>
2019-01-17 16:31:06 +0800

    逐帧动画

--- app/src/main/java/com/txm/topcodes/animationpracticeapplication/activity/ViewPropertyAnimationActivity.java
+++ app/src/main/java/com/txm/topcodes/animationpracticeapplication/activity/ViewPropertyAnimationActivity.java
@@ -2,8 +2,10 @@ package com.txm.topcodes.animationpracticeapplication.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,12 +21,15 @@ import com.txm.topcodes.animationpracticeapplication.base.BaseActivity;
  * Created by Tangxianming on 2019/1/17.
  * 视图、属性动画
  */
-public class ViewPropertyAnimationActivity extends BaseActivity implements Toolbar.OnMenuItemClickListener {
+public class ViewPropertyAnimationActivity extends BaseActivity implements Toolbar.OnMenuItemClickListener, View.OnClickListener {
 
 
     String title;
     private static final String TITLE_EXTRA = "titleExtra";
     ImageView ivTween;
+    ImageView ivFrame;
+    ConstraintLayout cslFrame;
+    AnimationDrawable animationDrawable;
 
 
     public static void start(Context context, String title) {
@@ -42,11 +47,16 @@ public class ViewPropertyAnimationActivity extends BaseActivity implements Toolb
     public void initListener() {
         title = getIntent().getStringExtra(TITLE_EXTRA);
         ivTween = findViewById(R.id.ivTween);
+        ivFrame = findViewById(R.id.ivFrame);
+        cslFrame = findViewById(R.id.cslFrame);
+        findViewById(R.id.btnStart).setOnClickListener(this);
+        findViewById(R.id.btnStop).setOnClickListener(this);
         initToolbar();
     }
 
     @Override
     public void initdata() {
+        animationDrawable= (AnimationDrawable) ivFrame.getDrawable();
         tweenAnimation();
     }
 
@@ -79,9 +89,13 @@ public class ViewPropertyAnimationActivity extends BaseActivity implements Toolb
     public boolean onMenuItemClick(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.action_tween://补间动画
+                cslFrame.setVisibility(View.GONE);
+                ivTween.setVisibility(View.VISIBLE);
                 tweenAnimation();
                 break;
             case R.id.action_frame://逐帧动画
+                cslFrame.setVisibility(View.VISIBLE);
+                ivTween.setVisibility(View.GONE);
                 frameAnimation();
                 break;
             case R.id.action_property://属性动画
@@ -103,7 +117,7 @@ public class ViewPropertyAnimationActivity extends BaseActivity implements Toolb
      * 逐帧动画
      */
     void frameAnimation() {
-
+        animationDrawable.start();
     }
 
     /**
@@ -114,4 +128,15 @@ public class ViewPropertyAnimationActivity extends BaseActivity implements Toolb
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnStart:
+                animationDrawable.start();
+                break;
+            case R.id.btnStop:
+                animationDrawable.stop();
+                break;
+        }
+    }
 }
