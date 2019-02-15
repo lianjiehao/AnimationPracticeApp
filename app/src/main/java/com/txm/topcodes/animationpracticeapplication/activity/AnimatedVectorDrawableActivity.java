c3d49dd83c0c9a10d36cbf8fb188f20ff137274f
kimball <tangxianming1994@gmail.com>
2019-02-15 10:53:18 +0800

    单文件矢量动画实现方式

--- app/src/main/java/com/txm/topcodes/animationpracticeapplication/activity/AnimatedVectorDrawableActivity.java
+++ app/src/main/java/com/txm/topcodes/animationpracticeapplication/activity/AnimatedVectorDrawableActivity.java
@@ -7,7 +7,10 @@ import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -18,9 +21,12 @@ import com.txm.topcodes.animationpracticeapplication.base.BaseActivity;
  * Created by Tangxianming on 2019/2/14.
  * 矢量图动画
  */
-public class AnimatedVectorDrawableActivity extends BaseActivity {
-    ImageView ivAnimation;
+public class AnimatedVectorDrawableActivity extends BaseActivity implements Toolbar.OnMenuItemClickListener {
     Toolbar toolbar;
+    ImageView ivAnimation01;
+    ImageView ivAnimation02;
+    ConstraintLayout cslAnimation01;
+    ConstraintLayout cslAnimation02;
     String title;
     private static final String TITLE_EXTRA = "titleExtra";
 
@@ -38,12 +44,24 @@ public class AnimatedVectorDrawableActivity extends BaseActivity {
 
     @Override
     public void initListener() {
+        cslAnimation01 = findViewById(R.id.cslAnimation01);
+        cslAnimation02 = findViewById(R.id.cslAnimation02);
         title = getIntent().getStringExtra(TITLE_EXTRA);
-        ivAnimation = findViewById(R.id.ivAnimation);
-        findViewById(R.id.btnStart).setOnClickListener(new View.OnClickListener() {
+        ivAnimation01 = findViewById(R.id.ivAnimation01);
+        findViewById(R.id.btnStart01).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Drawable drawable = ivAnimation.getDrawable();
+                Drawable drawable = ivAnimation01.getDrawable();//三个文件定义的矢量图动画
+                if (drawable instanceof Animatable) {
+                    ((Animatable) drawable).start();
+                }
+            }
+        });
+        ivAnimation02 = findViewById(R.id.ivAnimation02);
+        findViewById(R.id.btnStart02).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Drawable drawable = ivAnimation02.getDrawable();//单个文件定义的矢量图动画
                 if (drawable instanceof Animatable) {
                     ((Animatable) drawable).start();
                 }
@@ -63,11 +81,36 @@ public class AnimatedVectorDrawableActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//左侧添加一个默认的返回图标
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        toolbar.setOnMenuItemClickListener(this);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+    }
+
+    /**
+     * 创建菜单
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.vector_drawable, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_way_01:
+                cslAnimation01.setVisibility(View.VISIBLE);
+                cslAnimation02.setVisibility(View.GONE);
+                break;
+            case R.id.action_way_02:
+                cslAnimation01.setVisibility(View.GONE);
+                cslAnimation02.setVisibility(View.VISIBLE);
+                break;
+        }
+        return false;
     }
 }
