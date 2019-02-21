5358a28bb04087a77447cbcd53ad61b964c3f59e
kimball <tangxianming1994@gmail.com>
2019-02-21 12:31:35 +0800

    卡片翻转动画&揭露动画

--- app/src/main/java/com/txm/topcodes/animationpracticeapplication/activity/RevealOrHideActivity.java
+++ app/src/main/java/com/txm/topcodes/animationpracticeapplication/activity/RevealOrHideActivity.java
@@ -5,15 +5,21 @@ import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -22,6 +28,7 @@ import com.txm.topcodes.animationpracticeapplication.base.BaseActivity;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 /**
  * Created by Tangxianming on 2019/1/2.
@@ -34,7 +41,12 @@ public class RevealOrHideActivity extends BaseActivity implements Toolbar.OnMenu
     private View mContentView;
     private View mLoadingView;
     private int mShortAnimationDuration;
-    private Button btnShow;
+    boolean showingBack = false;
+    RelativeLayout rlCrossfade;
+    FrameLayout flContainer;
+    ImageView ivReveal;
+    RelativeLayout rlReveal;
+    Button btnStartReveal;
 
     public static void start(Context context, String title) {
         Intent starter = new Intent(context, RevealOrHideActivity.class);
@@ -49,26 +61,50 @@ public class RevealOrHideActivity extends BaseActivity implements Toolbar.OnMenu
 
     @Override
     public void initListener() {
-        logDebug("onCreate");
         title = getIntent().getStringExtra(TITLE_EXTRA);
-        initToolbar();
         mContentView = findViewById(R.id.content);
         mLoadingView = findViewById(R.id.loading_spinner);
+        rlCrossfade = findViewById(R.id.rlCrossfade);
+        flContainer = findViewById(R.id.flContainer);
+        ivReveal = findViewById(R.id.ivReveal);
+        rlReveal = findViewById(R.id.rlReveal);
+        btnStartReveal = findViewById(R.id.btnStartReveal);
         findViewById(R.id.btnShow).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 crossfade();
             }
         });
+        findViewById(R.id.flContainer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                flipCard();
+            }
+        });
+        btnStartReveal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Integer.valueOf(String.valueOf(btnStartReveal.getTag())) == 1) {
+                    revealInVisible();
+                } else {
+                    revealVisible();
+                }
+            }
+        });
     }
 
     @Override
     public void initdata() {
-        // Initially hide the content view.
+        initToolbar();
+        // 初始化crossfade相关
         mContentView.setVisibility(View.GONE);
-        // Retrieve and cache the system's default "short" animation time.
         mShortAnimationDuration = getResources().getInteger(
                 android.R.integer.config_shortAnimTime);
+        //初始化card flip相关
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.flContainer, new CardFrontFragment())
+                .commit();
     }
 
 
@@ -124,21 +160,146 @@ public class RevealOrHideActivity extends BaseActivity implements Toolbar.OnMenu
                 });
     }
 
+    private void flipCard() {
+        if (showingBack) {
+            getSupportFragmentManager().popBackStack();
+            return;
+        }
+
+        // Flip to the back.
+
+        showingBack = true;
+
+        // Create and commit a new fragment transaction that adds the fragment for
+        // the back of the card, uses custom animations, and is part of the fragment
+        // manager's back stack.
+
+        getSupportFragmentManager()
+                .beginTransaction()
+
+                // Replace the default fragment animations with animator resources
+                // representing rotations when switching to the back of the card, as
+                // well as animator resources representing rotations when flipping
+                // back to the front (e.g. when the system Back button is pressed).
+                .setCustomAnimations(
+                        R.animator.card_flip_right_in,
+                        R.animator.card_flip_right_out,
+                        R.animator.card_flip_left_in,
+                        R.animator.card_flip_left_out)
+
+                // Replace any fragments currently in the container view with a
+                // fragment representing the next page (indicated by the
+                // just-incremented currentPage variable).
+                .replace(R.id.flContainer, new CardBackFragment())
+
+                // Add this transaction to the back stack, allowing users to press
+                // Back to get to the front of the card.
+                .addToBackStack(null)
+
+                // Commit the transaction.
+                .commit();
+    }
+
+
+    private void revealVisible() {
+        // Check if the runtime version is at least Lollipop
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // get the center for the clipping circle
+            int cx = ivReveal.getWidth() / 2;
+            int cy = ivReveal.getHeight() / 2;
+
+            // get the final radius for the clipping circle
+            float finalRadius = (float) Math.hypot(cx, cy);
+
+            // create the animator for this view (the start radius is zero)
+            Animator anim = ViewAnimationUtils.createCircularReveal(ivReveal, cx, cy, 0f, finalRadius);
+
+            // make the view visible and start the animation
+            ivReveal.setVisibility(View.VISIBLE);
+            anim.start();
+        } else {
+            // set the view to visible without a circular reveal animation below Lollipop
+            ivReveal.setVisibility(View.VISIBLE);
+        }
+        btnStartReveal.setText("reveal to InVisible");
+        btnStartReveal.setTag("1");
+    }
+
+    private void revealInVisible() {
+        // Check if the runtime version is at least Lollipop
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // get the center for the clipping circle
+            int cx = ivReveal.getWidth() / 2;
+            int cy = ivReveal.getHeight() / 2;
+            // get the initial radius for the clipping circle
+            float initialRadius = (float) Math.hypot(cx, cy);
+
+            // create the animation (the final radius is zero)
+            Animator anim = ViewAnimationUtils.createCircularReveal(ivReveal, cx, cy, initialRadius, 0f);
+
+            // make the view invisible when the animation is done
+            anim.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    ivReveal.setVisibility(View.INVISIBLE);
+                }
+            });
+            // start the animation
+            anim.start();
+        } else {
+            // set the view to visible without a circular reveal animation below Lollipop
+            ivReveal.setVisibility(View.INVISIBLE);
+        }
+        btnStartReveal.setText("reveal to Visible");
+        btnStartReveal.setTag("2");
+    }
+
 
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.action_crossfade:
-
+                rlCrossfade.setVisibility(View.VISIBLE);
+                flContainer.setVisibility(View.GONE);
+                rlReveal.setVisibility(View.GONE);
                 break;
             case R.id.action_card_flip:
-
+                rlCrossfade.setVisibility(View.GONE);
+                flContainer.setVisibility(View.VISIBLE);
+                rlReveal.setVisibility(View.GONE);
                 break;
             case R.id.action_circular_reveal:
-
+                rlCrossfade.setVisibility(View.GONE);
+                flContainer.setVisibility(View.GONE);
+                rlReveal.setVisibility(View.VISIBLE);
                 break;
         }
         return false;
     }
+
+
+    /**
+     * A fragment representing the front of the card.
+     */
+    public static class CardFrontFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_front, container, false);
+        }
+    }
+
+    /**
+     * A fragment representing the back of the card.
+     */
+    public static class CardBackFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_back, container, false);
+        }
+    }
+
 
 }
