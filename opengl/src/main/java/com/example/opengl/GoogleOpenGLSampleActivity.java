5c3af078cf52a884b1689105cee045bd12d660e4
kimball <1428847194@qq.com>
2019-07-07 21:22:39 +0800

    google opengl es:环境配置、绘制三角形、使用projection和camera矫正图形

---
+++ opengl/src/main/java/com/example/opengl/GoogleOpenGLSampleActivity.java
@@ -0,0 +1,270 @@
+package com.example.opengl;
+
+import android.content.Context;
+import android.content.Intent;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.Bundle;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * 谷歌官方的OpenGL ES例子
+ */
+public class GoogleOpenGLSampleActivity extends AppCompatActivity {
+    private GLSurfaceView gLView;
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, GoogleOpenGLSampleActivity.class);
+        context.startActivity(starter);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = new MyGLSurfaceView(this);
+        setContentView(gLView);
+
+    }
+
+
+    static class MyGLSurfaceView extends GLSurfaceView {
+
+        private final MyGLRenderer renderer;
+
+        public MyGLSurfaceView(Context context) {
+            super(context);
+            // Create an OpenGL ES 2.0 context
+            setEGLContextClientVersion(2);
+            renderer = new MyGLRenderer();
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(renderer);
+            // Render the view only when there is a change in the drawing data
+            setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        }
+    }
+
+    static class MyGLRenderer implements GLSurfaceView.Renderer {
+        private Triangle mTriangle;
+        private Square mSquare;
+
+        // vPMatrix is an abbreviation for "Model View Projection Matrix"
+        private final float[] vPMatrix = new float[16];
+        private final float[] projectionMatrix = new float[16];
+        private final float[] viewMatrix = new float[16];
+
+        @Override
+        public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+            // Set the background frame color
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            // initialize a triangle
+            mTriangle = new Triangle();
+            // initialize a square
+            mSquare = new Square();
+        }
+
+        @Override
+        public void onDrawFrame(GL10 unused) {
+            // Redraw background color
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+            // Set the camera position (View matrix)
+            Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+            // Calculate the projection and view transformation
+            Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+            // Draw shape
+            mTriangle.draw(vPMatrix);
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 unused, int width, int height) {
+            GLES20.glViewport(0, 0, width, height);
+
+            float ratio = (float) width / height;
+
+            // this projection matrix is applied to object coordinates
+            // in the onDrawFrame() method
+            Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+        }
+
+        public static int loadShader(int type, String shaderCode) {
+
+            // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+            // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+            int shader = GLES20.glCreateShader(type);
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode);
+            GLES20.glCompileShader(shader);
+
+            return shader;
+        }
+    }
+
+
+    static class Triangle {
+
+        private FloatBuffer vertexBuffer;
+
+        // number of coordinates per vertex in this array
+        static final int COORDS_PER_VERTEX = 3;
+        static float triangleCoords[] = {   // in counterclockwise order:
+                0.0f, 0.622008459f, 0.0f, // top
+                -0.5f, -0.311004243f, 0.0f, // bottom left
+                0.5f, -0.311004243f, 0.0f  // bottom right
+        };
+
+        // Set color with red, green, blue and alpha (opacity) values
+        float color[] = {0.63671875f, 0.76953125f, 0.22265625f, 1.0f};
+
+
+        private final String vertexShaderCode =
+                // This matrix member variable provides a hook to manipulate
+                // the coordinates of the objects that use this vertex shader
+                "uniform mat4 uMVPMatrix;" +
+                        "attribute vec4 vPosition;" +
+                        "void main() {" +
+                        "  gl_Position = uMVPMatrix * vPosition;" +
+                        "}";
+
+        private final String fragmentShaderCode =
+                "precision mediump float;" +
+                        "uniform vec4 vColor;" +
+                        "void main() {" +
+                        "  gl_FragColor = vColor;" +
+                        "}";
+
+        private final int mProgram;
+
+        private int positionHandle;
+        private int colorHandle;
+        private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+        private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+        // Use to access and set the view transformation
+        private int vPMatrixHandle;
+
+        public Triangle() {
+            // initialize vertex byte buffer for shape coordinates
+            ByteBuffer bb = ByteBuffer.allocateDirect(
+                    // (number of coordinate values * 4 bytes per float)
+                    triangleCoords.length * 4);
+            // use the device hardware's native byte order
+            bb.order(ByteOrder.nativeOrder());
+
+            // create a floating point buffer from the ByteBuffer
+            vertexBuffer = bb.asFloatBuffer();
+            // add the coordinates to the FloatBuffer
+            vertexBuffer.put(triangleCoords);
+            // set the buffer to read the first coordinate
+            vertexBuffer.position(0);
+
+            int vertexShader = MyGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
+                    vertexShaderCode);
+            int fragmentShader = MyGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                    fragmentShaderCode);
+
+            // create empty OpenGL ES Program
+            mProgram = GLES20.glCreateProgram();
+
+            // add the vertex shader to program
+            GLES20.glAttachShader(mProgram, vertexShader);
+
+            // add the fragment shader to program
+            GLES20.glAttachShader(mProgram, fragmentShader);
+
+            // creates OpenGL ES program executables
+            GLES20.glLinkProgram(mProgram);
+
+        }
+
+        public void draw(float[] mvpMatrix) {
+            // Add program to OpenGL ES environment
+            GLES20.glUseProgram(mProgram);
+
+            // get handle to vertex shader's vPosition member
+            positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+            // Enable a handle to the triangle vertices
+            GLES20.glEnableVertexAttribArray(positionHandle);
+
+            // Prepare the triangle coordinate data
+            GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
+                    GLES20.GL_FLOAT, false,
+                    vertexStride, vertexBuffer);
+
+            // get handle to fragment shader's vColor member
+            colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+            // Set color for drawing the triangle
+            GLES20.glUniform4fv(colorHandle, 1, color, 0);
+
+            // get handle to shape's transformation matrix
+            vPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+            // Pass the projection and view transformation to the shader
+            GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0);
+
+            // Draw the triangle
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+            // Disable vertex array
+            GLES20.glDisableVertexAttribArray(positionHandle);
+
+        }
+
+    }
+
+
+    static class Square {
+
+        private FloatBuffer vertexBuffer;
+        private ShortBuffer drawListBuffer;
+
+        // number of coordinates per vertex in this array
+        static final int COORDS_PER_VERTEX = 3;
+        static float squareCoords[] = {
+                -0.5f, 0.5f, 0.0f,   // top left
+                -0.5f, -0.5f, 0.0f,   // bottom left
+                0.5f, -0.5f, 0.0f,   // bottom right
+                0.5f, 0.5f, 0.0f}; // top right
+
+        private short drawOrder[] = {0, 1, 2, 0, 2, 3}; // order to draw vertices
+
+        public Square() {
+            // initialize vertex byte buffer for shape coordinates
+            ByteBuffer bb = ByteBuffer.allocateDirect(
+                    // (# of coordinate values * 4 bytes per float)
+                    squareCoords.length * 4);
+            bb.order(ByteOrder.nativeOrder());
+            vertexBuffer = bb.asFloatBuffer();
+            vertexBuffer.put(squareCoords);
+            vertexBuffer.position(0);
+
+            // initialize byte buffer for the draw list
+            ByteBuffer dlb = ByteBuffer.allocateDirect(
+                    // (# of coordinate values * 2 bytes per short)
+                    drawOrder.length * 2);
+            dlb.order(ByteOrder.nativeOrder());
+            drawListBuffer = dlb.asShortBuffer();
+            drawListBuffer.put(drawOrder);
+            drawListBuffer.position(0);
+        }
+    }
+
+
+}
--- opengl/src/main/java/com/example/opengl/MainActivity.java
+++ opengl/src/main/java/com/example/opengl/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.opengl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btnGoogleSample).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoogleOpenGLSampleActivity.start(MainActivity.this);
+            }
+        });
     }
 }
